e23953b5f341cf4a2b211a9ac6ca23097a72a165
John-json <john.emil.johansson@live.com>
2025-03-04 03:39:16 +0100

    added yazi

--- .config/yazi/init.lua
+++ .config/yazi/init.lua
@@ -1,25 +1,10 @@
--- ~/.config/yazi/init.lua
-function Linemode:size_and_mtime()
-	local time = math.floor(self._file.cha.mtime or 0)
-	if time == 0 then
-		time = ""
-	elseif os.date("%Y", time) == os.date("%Y") then
-		time = os.date("%b %d %H:%M", time)
-	else
-		time = os.date("%b %d  %Y", time)
-	end
-
-	local size = self._file:size()
-	return string.format("%s %s", size and ya.readable_size(size) or "-", time)
-end
-
 require("yaziline"):setup({
-	color = "#c49f84",      -- main theme color
+	color = "#b7b7b7",      -- main theme color
 	separator_style = "curvy", -- "angly" | "curvy" | "liney" | "empty"
 	separator_open = "",
 	separator_close = "",
-	separator_open_thin = " ",
-	separator_close_thin = " ",
+	separator_open_thin = "",
+	separator_close_thin = "",
 	separator_head = "",
 	separator_tail = "",
 	select_symbol = "",
