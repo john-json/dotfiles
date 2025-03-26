d49465bd7e3f802fc50cf5565f01e087c565cd94
John-json <john.emil.johansson@live.com>
2025-03-26 07:29:02 +0100

    added install script

---
+++ .config/install.sh
@@ -0,0 +1,128 @@
+# Install xCode cli tools
+echo "Installing commandline tools..."
+xcode-select --install
+
+## Taps
+echo "Tapping Brew..."
+brew tap FelixKratz/formulae
+brew tap koekeishiya/formulae
+
+## Formulae
+echo "Installing Brew Formulae..."
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+brew analytics off
+export PATH="/opt/homebrew/bin:$PATH" >>~/.zshrc
+export eval "$(zoxide init zsh)" >>~/.zshrc
+
+#curls
+echo "Installing spotify terminal player"
+curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
+brew install spotify_player
+
+#colorls + gem
+sudo brew install ruby
+mkdir -p ~/.config/colorls
+gem install colorls
+
+### Essentials
+brew install spotify
+brew install node
+brew install sketchybar
+brew install git
+brew install yazi
+brew install tty-clock
+brew install mas
+brew install jq
+brew install tree
+
+### Terminal
+brew install neovim
+brew install zsh-autosuggestions
+brew install zsh-fast-syntax-highlighting
+brew install zoxide
+
+### Nice to have
+brew install lulu
+brew install btop
+brew install svim
+brew install lazygit
+
+## Casks
+echo "Installing Brew Casks..."
+brew install --cask readdle-spark
+brew install --cask logi-options+
+brew install --cask orion
+brew install --cask ghostty
+brew install --cask chatgpt
+brew install --cask ubersicht
+brew install --cask notunes
+
+### Fonts
+brew install --cask sf-symbols
+brew install --cask font-sf-mono
+brew install --cask font-sf-pro
+brew install --cask font-hack-nerd-font
+brew install --cask font-jetbrains-mono
+brew install --cask font-fira-code
+
+### Dev
+brew install --cask visual-studio-code
+
+# Mac App Store Apps
+echo "Installing Mac App Store Apps..."
+mas install 497799835  #xCode
+mas install 1251572132 #fresco
+
+# macOS Settings
+echo "Changing macOS defaults..."
+defaults write com.apple.NetworkBrowser BrowseAllInterfaces 1
+defaults write com.apple.desktopservices DSDontWriteNetworkStores -bool true
+defaults write com.apple.spaces spans-displays -bool false
+defaults write com.apple.dock autohide -bool true
+defaults write com.apple.dock "mru-spaces" -bool "false"
+defaults write NSGlobalDomain NSAutomaticWindowAnimationsEnabled -bool false
+defaults write com.apple.LaunchServices LSQuarantine -bool false
+defaults write NSGlobalDomain KeyRepeat -int 1
+defaults write NSGlobalDomain AppleShowAllExtensions -bool true
+defaults write NSGlobalDomain _HIHideMenuBar -bool true
+defaults write com.apple.screencapture location -string "$HOME/Pictures/Screenshots"
+defaults write com.apple.screencapture type -string "png"
+defaults write com.apple.finder ShowExternalHardDrivesOnDesktop -bool false
+defaults write com.apple.finder ShowHardDrivesOnDesktop -bool false
+defaults write com.apple.finder ShowMountedServersOnDesktop -bool false
+defaults write com.apple.finder ShowRemovableMediaOnDesktop -bool false
+defaults write com.apple.finder FXEnableExtensionChangeWarning -bool false
+defaults write com.apple.finder FXPreferredViewStyle -string "Nlsv"
+defaults write com.apple.finder ShowStatusBar -bool false
+defaults write com.apple.TimeMachine DoNotOfferNewDisksForBackup -bool YES
+defaults write com.apple.Safari AutoOpenSafeDownloads -bool false
+defaults write com.apple.Safari IncludeDevelopMenu -bool true
+defaults write com.apple.Safari WebKitDeveloperExtrasEnabledPreferenceKey -bool true
+defaults write com.apple.Safari com.apple.Safari.ContentPageGroupIdentifier.WebKit2DeveloperExtrasEnabled -bool true
+defaults write NSGlobalDomain WebKitDeveloperExtras -bool true
+defaults write com.apple.mail AddressesIncludeNameOnPasteboard -bool false
+defaults write -g NSWindowShouldDragOnGesture YES
+
+# Copying and checking out configuration files
+echo "Planting Configuration Files..."
+[ ! -d "$HOME/dotfiles" ] && git clone --bare github.com/john-json/dotfiles.git $HOME/dotfiles
+git --git-dir=$HOME/dotfiles/ --work-tree=$HOME checkout playground
+
+# Installing Fonts
+git clone git@github.com:shaunsingh/SFMono-Nerd-Font-Ligaturized.git /tmp/SFMono_Nerd_Font
+mv /tmp/SFMono_Nerd_Font/* $HOME/Library/Fonts
+rm -rf /tmp/SFMono_Nerd_Font/
+
+curl -L https://github.com/kvndrsslr/sketchybar-app-font/releases/download/v2.0.28/sketchybar-app-font.ttf -o $HOME/Library/Fonts/sketchybar-app-font.ttf
+
+source $HOME/.zshrc
+cfg config --local status.showUntrackedFiles no
+
+# Start Services
+echo "Starting Services (grant permissions)..."
+
+brew services start sketchybar
+
+csrutil status
+echo "(optional) Disable SIP."
+echo "Installation complete...\n"
