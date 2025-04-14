b5d6c3e2e92fff8323f16637291a8c5c3b4e86b5
John-json <john.emil.johansson@live.com>
2025-04-14 11:57:20 +0200

    added installer

--- .config/install.sh
+++ .config/install.sh
@@ -7,6 +7,34 @@ echo "Tapping Brew..."
 brew tap FelixKratz/formulae
 brew tap koekeishiya/formulae
 
+#sketchybar
+
+echo "Installing Dependencies"
+# Packages
+brew install lua
+brew install switchaudio-osx
+brew install nowplaying-cli
+
+brew tap FelixKratz/formulae
+brew install sketchybar
+
+# Fonts
+brew install --cask sf-symbols
+brew install --cask font-sf-mono
+brew install --cask font-sf-pro
+
+curl -L https://github.com/kvndrsslr/sketchybar-app-font/releases/download/v2.0.28/sketchybar-app-font.ttf -o $HOME/Library/Fonts/sketchybar-app-font.ttf
+
+# SbarLua
+(git clone https://github.com/FelixKratz/SbarLua.git /tmp/SbarLua && cd /tmp/SbarLua/ && make install && rm -rf /tmp/SbarLua/)
+
+echo "Cloning Config"
+git clone https://github.com/john-json/dotfiles.git /tmp/dotfiles
+mv $HOME/.config/sketchybar $HOME/.config/sketchybar_backup
+mv /tmp/dotfiles/.config/sketchybar $HOME/.config/sketchybar
+rm -rf /tmp/dotfiles
+brew services restart sketchybar
+
 ## Formulae
 echo "Installing Brew Formulae..."
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
@@ -34,6 +62,9 @@ brew install tty-clock
 brew install mas
 brew install jq
 brew install tree
+brew install ultralist
+brew install td
+brew install install spotify_player
 
 ### Terminal
 brew install neovim
---
+++ .config/install_sketchybar.sh
@@ -0,0 +1,25 @@
+echo "Installing Dependencies"
+# Packages
+brew install lua
+brew install switchaudio-osx
+brew install nowplaying-cli
+
+brew tap FelixKratz/formulae
+brew install sketchybar
+
+# Fonts
+brew install --cask sf-symbols
+brew install --cask font-sf-mono
+brew install --cask font-sf-pro
+
+curl -L https://github.com/kvndrsslr/sketchybar-app-font/releases/download/v2.0.28/sketchybar-app-font.ttf -o $HOME/Library/Fonts/sketchybar-app-font.ttf
+
+# SbarLua
+(git clone https://github.com/FelixKratz/SbarLua.git /tmp/SbarLua && cd /tmp/SbarLua/ && make install && rm -rf /tmp/SbarLua/)
+
+echo "Cloning Config"
+git clone https://github.com/john-json/dotfiles.git /tmp/dotfiles
+mv $HOME/.config/sketchybar $HOME/.config/sketchybar_backup
+mv /tmp/dotfiles/.config/sketchybar $HOME/.config/sketchybar
+rm -rf /tmp/dotfiles
+brew services restart sketchybar
